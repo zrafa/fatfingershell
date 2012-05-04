fb2935c68f0c680672732b4cc4fca084d29720bd
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2012-05-04 11:04:41 -0300

    Actualizamos la licencia apropiadamente

--- src/omshell.c
+++ src/omshell.c
@@ -3,10 +3,10 @@
  *
  * Copyright (C) 2009-2012 Rafael Ignacio Zurita <rizurita@yahoo.com>
  *
- *   FatFingerShell is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU Library General Public
- *   License as published by the Free Software Foundation;
- *   version 2 of the License.
+ *   FatFingerShell is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
  *
  *    You should have received a copy of the GNU Library General Public
  *    License along with this software (check COPYING); if not, write to the Free
